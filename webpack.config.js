d9502c07d6d46b168225dd4827396903ceb88453
Zhenya Tikhonov <masontikhonov@gmail.com>
2020-04-19 20:33:13 +0300

    refactor: add webpack & netlify

---
+++ webpack.config.js
@@ -0,0 +1,19 @@
+const htmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    filename: 'bundle.js',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+        },
+      },
+    ],
+  },
+};
